5d01918da65b524f0a6abacf5f66e18578c41a42
maruncho <legomarkata@gmail.com>
2025-05-31 20:50:01 +0300

    cleanup

---
+++ example_driver.sh
@@ -0,0 +1,48 @@
+#!/usr/bin/env bash
+
+file=""
+arg=""
+
+if [[ $1 == "" ]]; then
+    echo "File expected"; exit 1;
+elif [[ $2 == "" ]] && [[ "${1:0:2}" == "--" ]]; then
+    echo "File expected"; exit 1;
+elif [[ $2 == "" ]]; then
+    file=$1
+elif [[ "${1:0:2}" == "--" ]]; then
+    arg=$1;
+    file=$2;
+else
+    file=$1;
+    arg=$2;
+fi
+
+if [[ "${file: -2}" != ".c" ]]; then
+    echo "File must be .c"; exit 1;
+fi
+
+fileName="${file:0: -2}"
+
+gcc -E -P $file -o "$fileName.i"
+
+result="$?"
+if [[ $result != "0" ]]; then
+    exit $result
+fi
+
+./_build/default/bin/main.exe "$fileName.i" "$arg"
+result="$?"
+
+rm "$fileName.i"
+
+if [[ $arg != "" ]]; then
+    exit $result
+fi
+
+if [[ $result != "0" ]]; then
+    exit $result
+fi
+
+gcc "$fileName.s" -o $fileName
+
+rm "$fileName.s"
